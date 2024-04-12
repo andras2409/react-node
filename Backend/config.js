c7d75d86f814d369b8f7b4d54f0b4f833157a308
andras2409 <andras.nagy2002@gmail.com>
2024-04-12 20:06:38 +0200

    bug fixed

---
+++ Backend/config.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const mysql = require("mysql");
+
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:
+${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`
+
+const connection = mysql.createConnection(urlDB);
+
+module.exports = connection;
